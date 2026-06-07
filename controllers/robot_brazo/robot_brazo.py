dd710224d7c794afa0f436297a286ae350c3740f
Andres Bravo <andres01032001@gmail.com>
2026-06-07 18:13:45 -0500

    Limpiar proyecto: solo ROBOT1 con base y un brazo de 2 articulaciones

--- controllers/robot_brazo/robot_brazo.py
+++ controllers/robot_brazo/robot_brazo.py
@@ -15,40 +15,23 @@ sensor2.enable(timestep)
 motor2.setPosition(float('inf'))
 motor2.setVelocity(0.0)
 
-motor3 = robot.getDevice("motor_brazo_3")
-sensor3 = robot.getDevice("sensor_brazo_3")
-sensor3.enable(timestep)
-motor3.setPosition(float('inf'))
-motor3.setVelocity(0.0)
-
-motor4 = robot.getDevice("motor_brazo2_3")
-sensor4 = robot.getDevice("sensor_brazo2_3")
-sensor4.enable(timestep)
-motor4.setPosition(float('inf'))
-motor4.setVelocity(0.0)
-
 keyboard = Keyboard()
 keyboard.enable(timestep)
 
 while robot.step(timestep) != -1:
     key = keyboard.getKey()
-    motor1.setVelocity(0.0)
-    motor2.setVelocity(0.0)
-    motor3.setVelocity(0.0)
-    motor4.setVelocity(0.0)
     if key == Keyboard.UP:
         motor1.setVelocity(0.3)
+        motor2.setVelocity(0.0)
     elif key == Keyboard.DOWN:
         motor1.setVelocity(-0.3)
+        motor2.setVelocity(0.0)
     elif key == ord('A'):
         motor2.setVelocity(0.3)
+        motor1.setVelocity(0.0)
     elif key == ord('D'):
         motor2.setVelocity(-0.3)
-    elif key == ord('W'):
-        motor3.setVelocity(0.3)
-    elif key == ord('S'):
-        motor3.setVelocity(-0.3)
-    elif key == ord('E'):
-        motor4.setVelocity(0.3)
-    elif key == ord('Q'):
-        motor4.setVelocity(-0.3)
+        motor1.setVelocity(0.0)
+    else:
+        motor1.setVelocity(0.0)
+        motor2.setVelocity(0.0)
